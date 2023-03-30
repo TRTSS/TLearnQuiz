d10236ae3775acf926c221c0534ad8eb8622a603
Daniil Baikalov <felix.trof@gmail.com>
2023-03-30 21:15:02 +0300

    particles update 2

--- TLearnQuiz/static/js/effects.js
+++ TLearnQuiz/static/js/effects.js
@@ -36,7 +36,7 @@ class Vector {
 }
 
 class Particle {
-    constructor(pos, width, height, lifetime = 2, sprite = null, rotation = 0, die = new ParticleDieProcess(1, 1, 0), acceleration = 0, useGravity = false, gravityScale = 1) {
+    constructor(pos, width, height, lifetime = 2, sprite = null, rotation = 0, die = new ParticleDieProcess(1, 1, 0), acceleration = 0, useGravity = false, gravityScale = 1, debug = false) {
         this.pos = new Coord(pos.x, pos.y);
         this.width = width;
         this.height = height;
@@ -51,6 +51,7 @@ class Particle {
         this.acceleration = acceleration;
         this.useGravity = useGravity
         this.gravityScale = gravityScale;
+        this.debug = debug;
 
         setTimeout(() => {
             this.resizeSpeed = (die.resize - 1) / (this.lifetime - this.aliveTime / 1000);
@@ -59,7 +60,7 @@ class Particle {
 
         let represent = document.createElement("particle");
 
-        represent.style.transformOrigin = "center";
+        // represent.style.transformOrigin = "center";
         represent.style.position = "fixed";
 
         if (sprite == null) represent.style.background = "black";
@@ -74,7 +75,12 @@ class Particle {
         document.body.append(represent);
 
         this.representation = document.getElementById(represent.id);
-        this.representation.appendChild(this.representSprite);
+        if (sprite != null) this.representation.appendChild(this.representSprite);
+
+        if (this.debug) {
+            this.representation.classList.add ('debug-particle');
+        }
+
         this.setPosition(this.pos);
         this.setSize(width, height);
         this.setRotation(rotation);
@@ -119,8 +125,8 @@ class Particle {
     }
 
     setPosition(pos) {
-        this.representation.style.top = String(pos.y) + "px";
-        this.representation.style.left = String(pos.x) + "px";
+        this.representation.style.top = String(pos.y - this.height/2) + "px";
+        this.representation.style.left = String(pos.x - this.width/2) + "px";
     }
 
     setRotation(rotate) {
@@ -149,14 +155,68 @@ class Particle {
 class Emitter {
     constructor(data) {
         this.data = data;
+        this.debug = data.debug;
+        this.scale = 1;
+        this.pos = data.pos;
+
+
+
+        let represent = document.createElement("emitter");
+        represent.id = 'emitter-' + this.generateId();
+        document.body.append(represent);
+        this.representation = document.querySelector(`#${represent.id}`);
+
+        if (data.shape.form === 'circle') {
+            this.width = data.shape.radius;
+            this.height = data.shape.radius;
+        }
+
+        this.setSize(this.width, this.height);
+
+        let updateTimer = setInterval(() => {
+
+            this.updateEmitter();
+        }, FRAME_RATE);
+
+        if (this.debug) {
+            this.representation.classList.add('emitter-debug-circle');
+        }
+    }
+
+    setSize(width, height) {
+        this.representation.style.width = String(width * this.scale) + "px";
+        this.representation.style.height = String(height * this.scale) + "px";
+    }
+
+    setPosition(pos) {
+        this.representation.style.top = String(pos.y) + "px";
+        this.representation.style.left = String(pos.x) + "px";
+    }
+
+    generateId() {
+        let abc = 'qwertyuiopasdfghjklzxcvbnm';
+        let id = 'm';
+        while (id === 'm' && document.querySelector(`#emitter-${id}`) == null) {
+            id = '';
+            while (id.length < 10) {
+                id += abc[Math.round(Math.random() * (abc.length - 1))]
+            }
+        }
+        return id;
     }
 
     play() {
         let lifecycle = setInterval(() => {
-            let particle = new Particle(this.data.particle.pos, this.data.particle.width,
+            let particle = new Particle(this.pos, this.data.particle.width,
                  this.data.particle.height, this.data.particle.lifetime, this.data.particle.sprite,
-                this.data.particle.rotation, this.data.particle.die, this.data.particle.acceleration,
-                this.data.particle.gravity, this.data.particle.gravityScale);
+                0, this.data.particle.die, this.data.particle.acceleration,
+                this.data.particle.gravity, this.data.particle.gravityScale, this.data.particle.debug);
+            if (this.data.particle.rotation === "RANDOM") {
+                particle.setRotation(Math.random() * 360);
+            }
+            else {
+                particle.setRotation(this.data.particle.rotation);
+            }
             particle.radialVector = Math.random() * 360;
             particle.speed = this.data.particle.speed;
         }, 1000 / this.data.amount);
@@ -165,6 +225,11 @@ class Emitter {
             clearInterval(lifecycle);
             delete this;
         }, this.data.duration * 1000);
+    }
+
+    updateEmitter() {
+        this.setPosition(this.pos);
+        this.setSize(this.width, this.height);
     }
 }
 
